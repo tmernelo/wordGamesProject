2f528c47dc231f29f559740dddd3d367b32a2a05
James Barr <jbarr@sfsu.edu>
2025-05-02 20:52:20 -0700

    kept everything in method play?

--- Hangman.h
+++ Hangman.h
@@ -2,12 +2,16 @@
 #define HANGMAN_H
 
 #include "WordGame.h"
+#include <string>
+#include <vector>
 
 class Hangman : public WordGame {
+private: 
+    std::vector<std::string> asciiVector;
 public:
     Hangman(User* u);
-    void play() override;
-    bool gameOver() override;
+    void play();
+
 };
 
 #endif
