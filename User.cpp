23efdab5bef5c68f721ad993ed0d6aa03684f951
James Barr <jbarr@sfsu.edu>
2025-05-02 19:57:31 -0700

    User.cpp

--- User.cpp
+++ User.cpp
@@ -1,2 +1,25 @@
 #include "User.h"
 using namespace std;
+
+// Constructors + Initilization
+User::User() : score(0), name("Player1"), misses(0) {}
+User::User(std::string newName) : score(0), name(newName), misses(0) {}
+
+void User::setName(string username) {
+    name = username;
+}
+std::string User::getName() const {
+    return name;
+}
+
+// If you guess incorrectly
+void User::miss() {
+    misses++;
+}
+void User::addWin() {
+    score++;
+}
+
+int User::getScore() const {
+    return score;
+}
