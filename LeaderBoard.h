fd7358b9119091f5e8e590cebd200d76649c0fb7
emelysarcenobravo <emelybravo852@gmail.com>
2025-05-04 18:08:21 -0700

    Update LeaderBoard.h

--- LeaderBoard.h
+++ LeaderBoard.h
@@ -2,14 +2,16 @@
 #define LEADERBOARD_H
 
 #include <vector>
+#include "Hangman.h"
 #include "User.h"
+#include "Wordle.h"
 
 class LeaderBoard {
 private:
-    std::vector<User> topPlayers;
+    std::vector<User*> topPlayers;
 
 public:
-    void addPlayer(const User& player);
+    void addPlayer(User* player);
     void showTopPlayers() const;
 };
 
