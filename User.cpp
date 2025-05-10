7ea3d74c51fc395dac0548f2120942179ab02188
Emely Sarceno Bravo <emelybravo852@gmail.com>
2025-05-09 22:43:19 -0700

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -4,6 +4,7 @@ using namespace std;
 // Constructors + Initilization
 User::User() : score(0), name("Player1") {}
 User::User(std::string newName) : score(0), name(newName) {}
+User::User(std::string newName,int newScore): name(newName), score(newScore){}
 
 void User::setName(string username) {
     name = username;
