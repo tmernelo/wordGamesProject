bcdf07ca2970b015a8f759b0c230453c7459648c
Emely Sarceno Bravo <emelybravo852@gmail.com>
2025-05-11 23:28:29 -0700

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,17 +1,23 @@
 #include "Node.h"
+#include "User.h"
 
 // set node
 Node::Node() : data(""), next(nullptr), prev(nullptr) {}
 
 Node::Node(const std::string& word) : data(word), next(nullptr), prev(nullptr) {}
 
+Node::Node(const User& player): player(player), next(nullptr){}
+
+
 //getters
 Node* Node::getNext() const {return next; }
 Node* Node::getPrev() const {return prev; }
+User Node::getUser() const{ return player;}
 
 //setters
 void Node::setNext(Node* n) {next = n; }
 void Node::setPrev(Node* p) {prev = p; }
+void Node::setUser(const User& newPlayer){player = newPlayer;}
 
 std::string Node::getWord() const {return data; }
 
