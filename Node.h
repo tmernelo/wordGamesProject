a448ce317f14acf043c02edf9838171f5903b5b9
Emely Sarceno Bravo <emelybravo852@gmail.com>
2025-05-11 23:28:08 -0700

    Update Node.h

--- Node.h
+++ Node.h
@@ -1,27 +1,32 @@
 #ifndef NODE_H
 #define NODE_H
+#include "User.h"
 
 #include <string>
 
 struct Node {
    private:
+      User player;
       std::string data;
       Node* next;
       Node* prev;
       
    public:
-      Node();
-      
+      Node();  
       Node(const std::string& word);
+      Node(const User& player);
+
       
       //getters
       Node* getNext() const;
       Node* getPrev() const;
+      User getUser() const;
       
       //setters
       void setNext(Node* n);
       void setPrev(Node* p);
-      
+      void setUser(const User& newPlayer);
+
       std::string getWord() const;
       void setWord(std::string d);
 
